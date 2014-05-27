481a21613d54313362e818526ba5fdb18ba11935
rdinnager <r.dinnage@gmail.com>
2014-05-27 16:48:13 +0800

    made sperate data cleaning script

---
+++ inst/examples/cleanData.R
@@ -0,0 +1,13 @@
+
+#dat<-read.csv("D:/Users/Dinnage/Projects/WBHC-Project/data/COI_Play_Dataset.csv",stringsAsFactors=F)
+#dat<-dat[,c("processid","nucleotides","order_name","family_name","subfamily_name","genus_name","species_name")]
+#write.csv(dat,file="D:/Users/Dinnage/Projects/WBHC-Project/data/COI_Play_Dataset_clean.csv")
+
+
+dat<-read.csv("/home/din02g/Google Drive/WBHC-Project/data/COI_Play_Dataset.csv",stringsAsFactors=F)
+dat<-dat[,c("processid","nucleotides","order_name","family_name","subfamily_name","genus_name","species_name")]
+write.csv(dat,file="/home/din02g/Google Drive/WBHC-Project/data/COI_Play_Dataset_clean.csv")
+
+dat<-read.csv("/home/din02g/Google Drive/WBHC-Project/data/Train/COI_Train_Dataset.csv",stringsAsFactors=F)
+dat<-dat[,c("processid","nucleotides","order_name","family_name","subfamily_name","genus_name","species_name")]
+write.csv(dat,file="/home/din02g/Google Drive/WBHC-Project/data/COI_Play_Dataset_clean.csv")
