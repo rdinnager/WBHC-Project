9f55a701384574a0882b352649f1a58de2312868
rdinnager <r.dinnage@gmail.com>
2014-06-09 17:19:59 +0800

    combine RTDs

---
+++ inst/examples/combineRTDs.R
@@ -0,0 +1,14 @@
+## Script to load in RTD chunks for full COI sequences and concatenate them into one data file for further processing
+path <- "D:/Users/Dinnage/Projects/WBHC-Project/data/FullCOI/RTDs"
+numfiles <- 940
+library(data.table)
+library(dplyr)
+library(magrittr)
+
+getFiles <- function(i) {
+  fullpath <- paste(path, "/Hexapoda_COI_segments_RTD_", sprintf("%03d",i),".csv", sep="")
+  RTDs <- fread(fullpath) 
+  return(as.data.frame(RTDs))
+}
+
+RTDs <- seq_len(numfiles) %>% data.frame %>% set_names("filenum") %>% rowwise %>% do(getFiles(.$filenum))
