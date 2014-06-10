4e2e0bef6f1f3bd92b620987fd12f848ea139097
rdinnager <r.dinnage@gmail.com>
2014-06-10 16:47:24 +0800

    found faster way of reading data files using scan

--- inst/examples/combineRTDs.R
+++ inst/examples/combineRTDs.R
@@ -5,9 +5,15 @@ library(data.table)
 library(dplyr)
 library(magrittr)
 
+types <- read.csv(paste(path, "/Hexapoda_COI_segments_RTD_", sprintf("%03d",1),".csv", sep=""), nrows=2, 
+                          stringsAsFactors=FALSE, header=TRUE)[2,]
+types[7:10926]<-types[7:10926]+0.0123456789
+types<-as.list(types)
 getFiles <- function(i) {
   fullpath <- paste(path, "/Hexapoda_COI_segments_RTD_", sprintf("%03d",i),".csv", sep="")
-  RTDs <- read.csv(fullpath) 
+  RTDs <- scan(fullpath, what = types, sep=",", skip = 1) 
+  attr(RTDs, "row.names") <- .set_row_names(length(RTDs[[1]]))
+  class(RTDs) <- "data.frame"
   return(RTDs)
 }
 
